3e2136a7fdac233531902c25a06c1eeb517020d7
Gobinath Mallaiyan <i@gobinath.com>
2014-05-22 21:20:48 +0530

    Initial commit of GTD demo site with corporateclean

---
+++ README.md
@@ -0,0 +1,27 @@
+# Drupal Global Training Day - Demo Site
+
+Drupal Global Training Days is an initiative by the Drupal Association to introduce new and beginning users to Drupal. The Drupal Association is partnering with training companies to make this happen. Once a quarter, free or low cost training events will take place around the world with one of two curriculums:
+
+* "Introduction to Drupal," a full day training on the basics of Drupal. Attendees will leave having successfully built a Drupal site. It is ideal for those interested in exploring Drupal as a career path.
+* "What is Drupal?" This half-day workshop will address the basics of Drupal, and will give an overview to those interested in evaluating or implementing Drupal.
+Each training company can make it their own event and provide more detail on their web pages.
+
+We invite you to come Learn Drupal!
+
+## Installation & Usage instructions
+
++ Move contents of document_root folder to your websites root folder
++ Create an empty MySQL database and import there the “database/example_dev.sql” file.
++ open settings.php file in your favioute editor, navigate to line #213 where you will see a variable $databases. Change 'database', 'username', 'password' & 'host'
++ Now fire your favourite browser & visit http://localhost (or) the name you have give for your site
+
+Login to this site by using the following credentials:
+- u: admin
+- p: admin
+
+Demo of this can be found in [acquia cloud](http://globaltrainingdaydev.devcloud.acquia-sites.com/).
+
+    Developed & Maintained by [Gobinath](http://gobinath.com/).
+
+#### This demo is created with Drupal 7.28 and its out of the box components. 
+A theme which was not part of core is used [corporateclean](drupal.org/project/corporateclean)
---
+++ document_root/sites/all/themes/corporateclean/page.tpl.php
@@ -0,0 +1,279 @@
+<!-- #header -->
+<div id="header">
+	<!-- #header-inside -->
+    <div id="header-inside" class="container_12 clearfix">
+    	<!-- #header-inside-left -->
+        <div id="header-inside-left" class="grid_8">
+            
+            <?php if ($logo): ?>
+            <a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+            <?php endif; ?>
+     
+            <?php if ($site_name || $site_slogan): ?>
+            <div class="clearfix">
+            <?php if ($site_name): ?>
+            <span id="site-name"><a href="<?php print check_url($front_page); ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></span>
+            <?php endif; ?>
+            <?php if ($site_slogan): ?>
+            <span id="slogan"><?php print $site_slogan; ?></span>
+            <?php endif; ?>
+            </div>
+            <?php endif; ?>
+            
+        </div><!-- EOF: #header-inside-left -->
+        
+        <!-- #header-inside-right -->    
+        <div id="header-inside-right" class="grid_4">
+
+			<?php print render($page['search_area']); ?>
+
+        </div><!-- EOF: #header-inside-right -->
+    
+    </div><!-- EOF: #header-inside -->
+
+</div><!-- EOF: #header -->
+
+<!-- #header-menu -->
+<div id="header-menu">
+	<!-- #header-menu-inside -->
+    <div id="header-menu-inside" class="container_12 clearfix">
+    
+    	<div class="grid_12">
+            <div id="navigation" class="clearfix">
+            <?php if ($page['navigation']) :?>
+            <?php print drupal_render($page['navigation']); ?>
+            <?php else :
+            if (module_exists('i18n_menu')) {
+            $main_menu_tree = i18n_menu_translated_tree(variable_get('menu_main_links_source', 'main-menu'));
+            } else {
+            $main_menu_tree = menu_tree(variable_get('menu_main_links_source', 'main-menu')); 
+            }
+            print drupal_render($main_menu_tree);
+            endif; ?>
+            </div>
+        </div>
+        
+    </div><!-- EOF: #header-menu-inside -->
+
+</div><!-- EOF: #header-menu -->
+
+<!-- #banner -->
+<div id="banner">
+
+	<?php print render($page['banner']); ?>
+	
+    <?php if (theme_get_setting('slideshow_display','corporateclean')): ?>
+    
+    <?php if ($is_front): ?>
+    
+    <!-- #slideshow -->
+    <div id="slideshow">
+    
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12" align="center">
+                
+                <!--slider-item content-->
+                <img class="masked" src="<?php print base_path() . drupal_get_path('theme', 'corporateclean') ;?>/mockup/drupal-training-day-text.png"/>
+                <!--EOF:slider-item content-->
+				</div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12">
+                
+                <!--slider-item content-->
+                <div style="float:left; padding:0 30px 0 0;">
+                </div>
+                <h2>Drupal Association</h2>
+                <strong><a href="https://assoc.drupal.org/">https://assoc.drupal.org/</a></strong><br/>
+                <br/>
+                The Drupal Association is dedicated to helping the open-source Drupal CMS project flourish. We help the Drupal community with funding, infrastructure, education, promotion, distribution and online collaboration at Drupal.org. Funds to support these programs, and the Association staff come from memberships, supporting partners, sponsorships, donations, and DrupalCon events. Join us to help ensure a creative and exciting future for Drupal!
+                <div style="display:block; padding:30px 0 10px 0;"><a class="more" href="https://assoc.drupal.org/">Know More</a></div>
+                <!--EOF:slider-item content-->
+                
+                </div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+               
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12">
+                
+                <!--slider-item content-->
+				<div style="float:right; padding:0 0 0 30px;">
+                <img class="masked" src="<?php print base_path() . drupal_get_path('theme', 'corporateclean') ;?>/mockup/do_911_banner_large.png"/>
+                </div>					
+                <h2>Project Name : 9/11 Memorial & Museum</h2>
+                <strong>Category : Non-Profit</strong><br/> 
+                <br/>
+                911Memorial.org supports the National September 11 Memorial & Museum and honors the victims of the attacks on the World Trade Center. This case study details their new website with expanded functionality anchored in Drupal.
+                <div style="display:block; padding:30px 0 10px 0;"><a class="more" href="#">Tell me more</a></div>
+                <!--EOF:slider-item content-->
+                
+				</div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+        
+        <!--slider-item-->
+        <div class="slider-item">
+            <div class="content container_12">
+            	<div class="grid_12">
+                
+                <!--slider-item content-->
+                <div style="float:right; padding:0 0 0 30px;">
+                <img class="masked" src="<?php print base_path() . drupal_get_path('theme', 'corporateclean') ;?>/mockup/gta-case-study1_0.jpg"/>
+                </div>
+                <h2>Project Name : Georgia.gov</h2>
+                <strong>Category : Government</strong><br/>
+                <br/>
+                When 85 executive branch agencies and 1,400 state and local government entities depend on you for IT infrastructure and network services, efficient and cost effective flow of information is critical. Georgia Technology Authority (GTA) recognized that their expensive proprietary CMS would not provide a long-term enterprise content management solution and turned to Drupal to transition their websites’ current platform to a more flexible system.
+                <div style="display:block; padding:30px 0 10px 0;"><a class="more" href="https://drupal.org/node/1975200">Know More</a></div>
+                <!--EOF:slider-item content-->
+                
+				</div>
+            </div>
+        </div>
+        <!--EOF:slider-item-->
+    
+    </div>
+    <!-- EOF: #slideshow -->
+    
+    <!-- #slider-controls-wrapper -->
+    <div id="slider-controls-wrapper">
+        <div id="slider-controls" class="container_12">
+            <ul id="slider-navigation">
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- EOF: #slider-controls-wrapper -->
+    
+    <?php endif; ?>
+    
+	<?php endif; ?>  
+
+</div><!-- EOF: #banner -->
+
+
+<!-- #content -->
+<div id="content">
+	<!-- #content-inside -->
+    <div id="content-inside" class="container_12 clearfix">
+    
+        <?php if ($page['sidebar_first']) :?>
+        <!-- #sidebar-first -->
+        <div id="sidebar-first" class="grid_4">
+        	<?php print render($page['sidebar_first']); ?>
+        </div><!-- EOF: #sidebar-first -->
+        <?php endif; ?>
+        
+        <?php if ($page['sidebar_first'] && $page['sidebar_second']) { ?>
+        <div class="grid_4">
+        <?php } elseif ($page['sidebar_first'] || $page['sidebar_second']) { ?>
+        <div id="main" class="grid_8">
+		<?php } else { ?>
+        <div id="main" class="grid_12">    
+        <?php } ?>
+            
+            <?php if (theme_get_setting('breadcrumb_display','corporateclean')): print $breadcrumb; endif; ?>
+            
+            <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+       
+            <?php if ($messages): ?>
+            <div id="console" class="clearfix">
+            <?php print $messages; ?>
+            </div>
+            <?php endif; ?>
+     
+            <?php if ($page['help']): ?>
+            <div id="help">
+            <?php print render($page['help']); ?>
+            </div>
+            <?php endif; ?>
+            
+            <?php if ($action_links): ?>
+            <ul class="action-links">
+            <?php print render($action_links); ?>
+            </ul>
+            <?php endif; ?>
+            
+			<?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+            <h1><?php print $title ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            
+            <?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?>
+            
+            <?php print render($page['content']); ?>
+            
+            <?php print $feed_icons; ?>
+            
+        </div><!-- EOF: #main -->
+        
+        <?php if ($page['sidebar_second']) :?>
+        <!-- #sidebar-second -->
+        <div id="sidebar-second" class="grid_4">
+        	<?php print render($page['sidebar_second']); ?>
+        </div><!-- EOF: #sidebar-second -->
+        <?php endif; ?>  
+
+    </div><!-- EOF: #content-inside -->
+
+</div><!-- EOF: #content -->
+
+<!-- #footer -->    
+<div id="footer">
+	<!-- #footer-inside -->
+    <div id="footer-inside" class="container_12 clearfix">
+    
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_first']); ?>
+        </div><!-- EOF: .footer-area -->
+        
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_second']); ?>
+        </div><!-- EOF: .footer-area -->
+        
+        <div class="footer-area grid_4">
+        <?php print render($page['footer_third']); ?>
+        </div><!-- EOF: .footer-area -->
+       
+    </div><!-- EOF: #footer-inside -->
+
+</div><!-- EOF: #footer -->
+
+<!-- #footer-bottom -->    
+<div id="footer-bottom">
+
+	<!-- #footer-bottom-inside --> 
+    <div id="footer-bottom-inside" class="container_12 clearfix">
+    	<!-- #footer-bottom-left --> 
+    	<div id="footer-bottom-left" class="grid_8">
+        
+            
+            <?php print render($page['footer']); ?>
+            
+        </div>
+    	<!-- #footer-bottom-right --> 
+        <div id="footer-bottom-right" class="grid_4">
+        
+        	<?php print render($page['footer_bottom_right']); ?>
+        
+        </div><!-- EOF: #footer-bottom-right -->
+       
+    </div><!-- EOF: #footer-bottom-inside -->
+
+</div><!-- EOF: #footer -->
